e3a4d20d12e92aea88f2ee4e8ae81aca46cfbbc6
zhangyu <grewer@grewer.cn>
2020-07-23 00:31:14 +0800

    feat: 时间添加

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
-const path = require('path');
-
 require("dotenv").config();
-
-const Octokit = require("@octokit/rest");
+const {WakaTimeClient, RANGE} = require("wakatime-client");
+const fs = require('fs');
 
 const {
-    GIST_ID: gistId,
-    GH_TOKEN: githubToken,
+    WAKATIME_API_KEY: wakatimeApiKey
 } = process.env;
 
-console.log('githubToken',githubToken)
+const wakatime = new WakaTimeClient(wakatimeApiKey);
 
-const octokit = new Octokit({auth: `token ${githubToken}`});
+const root = process.cwd()
 
-// octokit.repos.createOrUpdateFile()
-const main = async ()=>{
-   const readme = await octokit.repos.getReadme({
-        owner: 'grewer',
-        repo: 'grewer',
-    });
+const main = async () => {
+    const stats = await wakatime.getMyStats({range: RANGE.LAST_7_DAYS});
+    const timeContent = getTimeContent(stats)
+    console.log(timeContent)
 
-    console.log(readme)
+    const template = fs.readFileSync(`${root}/template.md`, {encoding: 'utf-8'})
+
+    console.log(template, typeof template)
+
+    const _template = template.replace(/#Time#/, `\`\`\`text \n${timeContent.join('\n')} \n \`\`\``)
+
+    fs.writeFileSync(`${root}/README.md`, _template)
+
 }
 
+
+const getTimeContent = (stats) => {
+
+    const lines = [];
+    for (let i = 0; i < Math.min(stats.data.languages.length, 5); i++) {
+        const data = stats.data.languages[i];
+        const {name, percent, text: time} = data;
+
+        const line = [
+            name.padEnd(11),
+            time.padEnd(14),
+            generateBarChart(percent, 21),
+            String(percent.toFixed(1)).padStart(5) + "%"
+        ];
+
+        lines.push(line.join(" "));
+    }
+
+    return lines
+
+}
+
+
+function generateBarChart(percent, size) {
+    const syms = "░▏▎▍▌▋▊▉█";
+
+    const frac = Math.floor((size * 8 * percent) / 100);
+    const barsFull = Math.floor(frac / 8);
+    if (barsFull >= size) {
+        return syms.substring(8, 9).repeat(size);
+    }
+    const semi = frac % 8;
+
+    return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
+        .join("")
+        .padEnd(size, syms.substring(0, 1));
+}
 
 
 main()
---
+++ template.md
@@ -0,0 +1,13 @@
+## Hello 😜
+<table>
+<tr>
+<td valign="top" width="50%">
+#Time#
+</td>
+<td valign="top" width="50%">
+待添加
+</td>
+</tr>
+
+
+[![HitCount](http://hits.dwyl.com/grewer@grewercn/Grew'er.svg)](http://hits.dwyl.com/grewer@grewercn/Grew'er)
